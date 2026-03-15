edaa188247fd4d29c0714c4013a530c622f4f998
Kafaa Billahi Syahida <kafaa.developer@gmail.com>
2026-03-15 08:36:17 +0700

    try to do loop unrolling

--- app/Parser.php
+++ app/Parser.php
@@ -83,6 +83,47 @@ final class Parser
                 $comma = strpos($chunk, ',', $pos);
                 $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
                 $pos = $comma + 55;
+
+                if ($pos >= $lastNewline) break;
+                $comma = strpos($chunk, ',', $pos);
+                $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
+                $pos = $comma + 55;
+
+                if ($pos >= $lastNewline) break;
+                $comma = strpos($chunk, ',', $pos);
+                $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
+                $pos = $comma + 55;
+
+                if ($pos >= $lastNewline) break;
+                $comma = strpos($chunk, ',', $pos);
+                $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
+                $pos = $comma + 55;
+
+                if ($pos >= $lastNewline) break;
+                $comma = strpos($chunk, ',', $pos);
+                $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
+                $pos = $comma + 55;
+
+                if ($pos >= $lastNewline) break;
+                $comma = strpos($chunk, ',', $pos);
+                $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
+                $pos = $comma + 55;
+
+                if ($pos >= $lastNewline) break;
+                $comma = strpos($chunk, ',', $pos);
+                $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
+                $pos = $comma + 55;
+
+                if ($pos >= $lastNewline) break;
+                $comma = strpos($chunk, ',', $pos);
+                $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
+                $pos = $comma + 55;
+            }
+
+            while ($pos < $lastNewline) {
+                $comma = strpos($chunk, ',', $pos);
+                $counts[$slugIds[substr($chunk, $pos, $comma - $pos)] + $dateIds[substr($chunk, $comma + 4, 7)]]++;
+                $pos = $comma + 55;
             }
 
             $leftover = strlen($chunk) - $lastNewline - 1;
